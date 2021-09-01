604ffe9c725b661bc20f36bca9f26fe9f264502b
Andre Artus <andre.artus@gmail.com>
2021-09-01 06:22:44 +0200

    Initial commit

---
+++ core/src/main/java/com/example/tur/MyClass.kt
@@ -0,0 +1,10 @@
+package com.example.tur
+import java.util.*
+
+class MyClass {
+    fun doSomethingRandom(): Unit {
+
+        val nextInt = Random().nextInt(10)
+        "  Hello World ".trim()
+    }
+}
